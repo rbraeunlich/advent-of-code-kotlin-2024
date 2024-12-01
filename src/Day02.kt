6031f2abf2aaead8ea8845538422c581c804fabe
Ronny Bräunlich <ronny.braeunlich@cbc.de>
2024-12-01 09:33:28 +0100

    Prepare Day 2

---
+++ src/Day02.kt
@@ -0,0 +1,21 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
---
+++ src/DayXX.kt
@@ -0,0 +1,21 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("DayXX_test")
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("DayXX")
+    part1(input).println()
+    part2(input).println()
+}
